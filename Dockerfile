d7dbbd53e0cc2b4c6faeed9681cd00bb45e644b8
Omer Gelbard <omer@capitalos.co>
2023-09-24 23:44:22 +0200

    copy the custom entrypoint file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM appsmith/appsmith-ce:v1.9.37.1
 
+WORKDIR /opt/appsmith
+
+COPY custom_entrypoint.sh .
+
 RUN chmod +x custom_entrypoint.sh
 
 ENTRYPOINT [ "/opt/appsmith/custom_entrypoint.sh" ]
--- custom_entrypoint.sh
+++ custom_entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# Needed until https://github.com/appsmithorg/appsmith/pull/27461 is released
 sed -i 's/localhost\//localhost:8091\//' /opt/appsmith/run-java.sh
 
 /opt/appsmith/entrypoint.sh /usr/bin/supervisord -n
