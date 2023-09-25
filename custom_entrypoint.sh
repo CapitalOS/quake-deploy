59129232fd04614585fc63c3ba7fb6db7a5a3d04
Omer Gelbard <omer@capitalos.co>
2023-09-25 23:42:08 +0200

    try to force the server to listen on port 80, so render can run health-check

--- custom_entrypoint.sh
+++ custom_entrypoint.sh
@@ -5,4 +5,7 @@ set -e
 # Needed until https://github.com/appsmithorg/appsmith/pull/27461 is released
 sed -i 's/localhost\//localhost:8091\//' /opt/appsmith/run-java.sh
 
+# Needed to support render internal healthcheck, since render will try to access the app on port 80 to check for healthcheck
+sed -i '/listen.*default_server;/a \tlisten 80 default_server;' /opt/appsmith/templates/nginx-app.conf.sh
+
 /opt/appsmith/entrypoint.sh /usr/bin/supervisord -n
